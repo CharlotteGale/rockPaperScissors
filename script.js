a069a10effa346f2665182191aa0aa4c0518ede5
Charlotte Gale <CharlotteMGale@outlook.com>
2024-12-20 12:19:23 +0000

    Add reset button etc
    Add reset button to index.html
    Write logic for resetGame() script.js
    Add styling to reset button in styles.css

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 document.getElementById("rock").addEventListener("click", () => { playRound('rock'); });
 document.getElementById("paper").addEventListener("click", () => { playRound('paper') });
 document.getElementById("scissors").addEventListener("click", () => { playRound('scissors') });
+document.getElementById("reset").addEventListener("click", () => { resetGame() });
 
 function getComputerChoice() {
      const randomNumber = Math.floor(Math.random() *3)+1
@@ -11,12 +12,37 @@ function getComputerChoice() {
      } else {
           return "scissors";
      }
-}
-
+};
+let gameStarted = false;
 let userScore = 0;
 let computerScore = 0;
 
+function announceScore(userScore, computerScore) {
+          if (userScore == computerScore) {
+               document.getElementById("announceWinner").innerText=("The game was a tie!")
+          } else if (userScore > computerScore) {
+               document.getElementById("announceWinner").innerText=("You beat the machine!")
+          } else {
+               document.getElementById("announceWinner").innerText=("You lost. Better luck next time")
+          }
+     setTimeout(() => {
+          resetGame();
+     }, 60000);
+};
+
+function resetGame() {
+     userScore = 0;
+     computerScore = 0;
+     document.getElementById("score").innerText = "Your score: 0. Computer score: 0";
+     document.getElementById("result").innerHTML = "";
+     document.getElementById("announceWinner").innerText = "Best of 5";
+     gameStarted = false;
+};
+
 function playRound(userChoice) {
+     if (!gameStarted) {
+          gameStarted = true;
+     }
      let computerChoice = getComputerChoice();
      if (userChoice === computerChoice) {
           document.getElementById("result").innerHTML=("It's a tie!");
@@ -31,7 +57,9 @@ function playRound(userChoice) {
           document.getElementById("result").innerHTML=(`You lose! Your ${userChoice} loses to ${computerChoice}`);
           computerScore++;
      }
-     document.getElementById("score").innerHTML=(`Your score: ${userScore}. Computer score: ${computerScore}`);
-}
-
-playRound();
+     document.getElementById("score").innerText=(`Your score: ${userScore}. Computer score: ${computerScore}`);
+     
+     if (userScore === 5 || computerScore === 5) {
+          announceScore(userScore, computerScore);
+     };
+};
